1e7b8b154ce9bf15047351d4dcc80566594d48de
David Cook <divergentdave@gmail.com>
2016-06-14 20:32:42 -0500

    Preserve dates during build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm -rf deploy
 mkdir deploy
 bower install
-cp bower_components/normalize.css/normalize.css css
-cp bower_components/milligram/dist/milligram.min.css css
+cp -p bower_components/normalize.css/normalize.css css
+cp -p bower_components/milligram/dist/milligram.min.css css
 .virtualenv/bin/python extract.py
-cp -R css data fireworks.ics fonts index.html deploy
+cp -pR css data fireworks.ics fonts index.html deploy

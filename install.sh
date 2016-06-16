dae012cfedfbde69da47fc408e612ead1d9b9785
David Cook <divergentdave@gmail.com>
2016-06-15 23:52:02 -0500

    Use Python 3 in virtualenv

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ TOPLEVEL=$(git rev-parse --show-toplevel)
 if [ ! -d "$TOPLEVEL/.virtualenv" ]
 then
     python3 -m virtualenv --version >/dev/null || { sudo pip install virtualenv; }
-    python3 -m virtualenv "$TOPLEVEL/.virtualenv"
+    python3 -m virtualenv -p python3 "$TOPLEVEL/.virtualenv"
 fi
 "$TOPLEVEL/.virtualenv/bin/pip" install -r "$TOPLEVEL/requirements.txt"
 if [ ! -f "$TOPLEVEL/.git/hooks/pre-commit" ]

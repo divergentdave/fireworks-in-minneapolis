e5ad53df76070319c4544c336aff8e912eb130e1
David Cook <divergentdave@gmail.com>
2016-06-30 21:53:05 -0500

    July 2016 data production, major script changes
    Data came in a different format, plus the MPRB's July 4th event didn't
    show up in permit data, which required a separate workflow.

--- extract.py
+++ extract.py
@@ -9,22 +9,31 @@ import icalendar
 import jinja2
 import openpyxl
 import titlecase
+import yaml
 
 DIRECTORY = "data"
 ICS_PATH = "fireworks.ics"
 DATE_RE = re.compile("(January|February|March|April|May|June|July|August|"
                      "September|October|November|December) "
-                     "([0-9]{1,2}(?:, [0-9]{1,2})*), (20[0-9]{2})", re.I)
+                     "([0-9]{1,2}(?:, [0-9]{1,2})*)(?:st|nd|rd|th)?, "
+                     "(20[0-9]{2})", re.I)
 TIMEZONE = pytz.timezone("US/Central")
+
+
+def maybe_titlecase(val):
+    if val is None:
+        return val
+    else:
+        return titlecase.titlecase(val)
 
 
 class Permit(object):
     def __init__(self, number, name, description, address, comment):
         self.number = number
-        self.name = titlecase.titlecase(name)
+        self.name = maybe_titlecase(name)
         self.description = description
-        self.address = titlecase.titlecase(address)
-        if "MINNEAPOLIS" not in self.address.upper():
+        self.address = maybe_titlecase(address)
+        if self.address and "MINNEAPOLIS" not in self.address.upper():
             self.address = self.address + ", Minneapolis, MN"
         self.comment = comment
 
@@ -82,8 +91,18 @@ class Event(object):
         event.add("description", self.comment)
         return event
 
+    def __str__(self):
+        return "%s, \"%s\", %s, \"%s\", \"%s\"" % (self.uid,
+                                                   self.name,
+                                                   self.date,
+                                                   self.address,
+                                                   self.comment)
+
     def __lt__(self, other):
-        return self.date < other.date
+        if self.date != other.date:
+            return self.date < other.date
+        else:
+            return self.uid < other.uid
 
 
 def is_blank_row(row):
@@ -98,28 +117,53 @@ def text_lookup(cell):
     if value is None:
         return None
     else:
-        return re.sub("\\s+", " ", value).strip()
+        return re.sub("\\s+", " ", str(value)).strip()
 
 
 def parse_spreadsheet(path):
     workbook = openpyxl.load_workbook(path)
     worksheet = workbook.active
 
-    header_row = [cell.value.strip() for cell in worksheet.rows[1]]
-    number_index = header_row.index("Permit Number")
-    name_index = header_row.index("Permit Name")
-    description_index = header_row.index("Description")
-    address_index = header_row.index("Permit Street Address")
-    comment_index = header_row.index("Comment Text")
+    header_row = [text_lookup(cell) for cell in worksheet.rows[1]]
+    if path.endswith("201606.xlsx"):
+        number_index = header_row.index("Permit Number")
+        name_index = header_row.index("Permit Name")
+        description_index = header_row.index("Description")
+        address_index = header_row.index("Permit Street Address")
+        comment_index = header_row.index("Comment Text")
+    elif path.endswith("201607.xlsx"):
+        number_index = header_row.index("Permit#")
+        name_index = header_row.index("Event")
+        description_index = None
+        address_index = None
+        comment_index = header_row.index("Event Details")
 
     for row in worksheet.rows[2:]:
         if is_blank_row(row):
             continue
-        permit = Permit(text_lookup(row[number_index]),
-                        text_lookup(row[name_index]),
-                        text_lookup(row[description_index]),
-                        text_lookup(row[address_index]),
-                        text_lookup(row[comment_index]))
+        permit_number = text_lookup(row[number_index])
+        permit_name = text_lookup(row[name_index])
+        permit_comment = text_lookup(row[comment_index])
+        if description_index:
+            permit_description = text_lookup(row[description_index])
+        else:
+            permit_description = None
+        if address_index:
+            permit_address = text_lookup(row[address_index])
+        elif permit_comment:
+            match = re.search("THE EVENT ADDRESS: ([^.]+)\\. THE EVENT",
+                              permit_comment)
+            if match:
+                permit_address = match.group(1)
+            else:
+                permit_address = None
+        else:
+            permit_address = None
+        permit = Permit(permit_number,
+                        permit_name,
+                        permit_description,
+                        permit_address,
+                        permit_comment)
         yield permit
 
 
@@ -127,6 +171,10 @@ def filter_unwanted_permits(permits):
     for permit in permits:
         if (permit.description == "FIREWORKS DISPLAY - ONE TIME" and
                 "INDOOR" in permit.comment):
+            continue
+        if permit.number == "165175":
+            # only a permit for temporary LP, etc.
+            # this data production didn't include a proper description column
             continue
         else:
             yield permit
@@ -170,9 +218,29 @@ def permits_to_events(permits):
         yield from permit.event_iter()
 
 
+def load_extra_events():
+    data = yaml.load(open(os.path.join(DIRECTORY, "other.yaml")))
+    for id_, obj in data.items():
+        uid = "fireworks-%s@davidsherenowitsa.party" % id_
+        date = datetime.datetime.strptime(obj["date"], "%Y-%m-%d").date()
+        yield Event(uid, obj["name"], date, obj["address"], obj["comment"])
+
+
 def main():
-    events = list(permits_to_events(deduplicate(parse_data())))
+    permits = deduplicate(parse_data())
+
+    print("Permits:")
+    for permit in permits:
+        print(permit)
+    print()
+
+    events = list(permits_to_events(permits)) + list(load_extra_events())
     events.sort()
+
+    print("Events:")
+    for event in events:
+        print(event)
+
     write_icalendar(events)
     write_html(events)
 
